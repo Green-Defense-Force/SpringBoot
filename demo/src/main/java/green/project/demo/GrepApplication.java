751d56bd479f8e1b5c3c118041ae29e53dffaa6e
bong94688 <bong94688@gmail.com>
2023-11-15 21:13:50 +0900

    feat - 0: spring boot 초기파일

---
+++ demo/src/main/java/green/project/demo/GrepApplication.java
@@ -0,0 +1,13 @@
+package green.project.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrepApplication.class, args);
+	}
+
+}
---
+++ demo/src/test/java/green/project/demo/GrepApplicationTests.java
@@ -0,0 +1,13 @@
+package green.project.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrepApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
