6945a1901fa164b8f1abdef2f800db94065e7bc3
GALAXYBOOK\USER <bong94688@gmail.com>
2023-12-21 11:20:01 +0900

    feat-34: Swagger 설정추가 Maincontroller,SwagerConfiguration 추가

--- src/main/java/green/project/demo/configuration/SwaggerConfiguration.java
+++ src/main/java/green/project/demo/configuration/SwaggerConfiguration.java
@@ -1,71 +1,25 @@
 package green.project.demo.configuration;
 
-import java.util.ArrayList;
-
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
 @Configuration
-@EnableSwagger2
 public class SwaggerConfiguration {
 
 	@Bean
-	public Docket api() {
-
-		return new Docket(DocumentationType.SWAGGER_2)
-			.groupName("Version1")
-			.select()
-			.apis(RequestHandlerSelectors.
-				basePackage("green.project.demo"))
-			.paths(PathSelectors.ant("/v1/api/**"))
-			.build()
-			.apiInfo(apiInfo());
+	public OpenAPI openAPI() {
+		return new OpenAPI()
+			.components(new Components())
+			.info(apiInfo());
 	}
 
-	@Bean
-	public Docket apiV1() {
-
-		return new Docket(DocumentationType.SWAGGER_2)
-			.groupName("Version1")
-			.select()
-			.apis(RequestHandlerSelectors.
-				basePackage("green.project.demo"))
-			.paths(PathSelectors.ant("/v1/api/**"))
-			.build()
-			.apiInfo(apiInfo());
-	}
-
-	@Bean
-	public Docket apiV2() {
-		return new Docket(DocumentationType.SWAGGER_2)
-			.useDefaultResponseMessages(false)
-			.groupName("Version1")
-			.select()
-			.apis(RequestHandlerSelectors.
-				basePackage("green.project.demo"))
-			.paths(PathSelectors.ant("/v2/api/**"))
-			.build()
-			.apiInfo(apiInfo());
-	}
-
-	private ApiInfo apiInfo() {
-		return new ApiInfo(
-			"Title",
-			"Description",
-			"version 1.0",
-			"https://naver.com",
-			new Contact("Contact Me", "https://daum.net", "colt@colt.com"),
-			"Edit Licenses",
-			"https://naver.com",
-			new ArrayList<>()
-		);
+	private Info apiInfo() {
+		return new Info()
+			.title("Springdoc 테스트")
+			.description("Springdoc을 사용한 Swagger UI 테스트")
+			.version("1.0.0");
 	}
 }
